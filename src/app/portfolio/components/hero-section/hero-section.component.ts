9819b66e33718f13924ee6214f50b259bd521c36
a13xandr3 <estevesalves@icloud.com>
2026-09-17 14:53:10 -0300

    feat: redesign hero with profile assets

--- src/app/portfolio/components/hero-section/hero-section.component.ts
+++ src/app/portfolio/components/hero-section/hero-section.component.ts
@@ -1,9 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'section[appHeroSection]',
-  imports: [MatButtonModule],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
